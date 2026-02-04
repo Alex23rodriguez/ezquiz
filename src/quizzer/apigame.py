4505e5f1696145d111347a434b13690abb18a141
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-02-04 18:12:51 +0100

    feat: apigame template

---
+++ src/quizzer/apigame.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from random import choice
+from typing import Literal
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+
+from quizzer.quizzer import Q
+
+
+class APIGame:
+    def __init__(self, title: str, qs: dict[str, Q]) -> None:
+        self.title = title
+        self.qs = qs
+        self.score = 0
+        self.mistakes = 0
+        self.streak = 0
+        self.history = []
+
+    def start(
+        self,
+        mode: Literal["timed", "score", "mistakes", "total", "streak"] = "score",
+        param=10,
+    ):
+        app = FastAPI()
+
+        templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
+
+        @app.get("/", response_class=HTMLResponse)
+        async def landing_page(request: Request):
+            return templates.TemplateResponse(
+                "index.html",
+                context={
+                    "request": request,
+                    "title": self.title,
+                    "categories": list(self.qs.keys()),
+                },
+            )
+
+        @app.post("/api/next", response_class=JSONResponse)
+        async def next_question(request: Request):
+            """Get the next question in the session."""
+            # return a random question from selected categories
+            data = await request.json()
+            categories = data.get("categories", [])
+
+            # Get all available questions from selected categories
+            cat = choice(categories)
+
+            q = self.qs[cat]
+            seed = q.get_seed()
+            prompt = q.ask(seed)
+            print(prompt)
+
+            # available = []
+            # for category in categories:
+            #     if category in MOCK_QUESTIONS:
+            #         available.extend(MOCK_QUESTIONS[category])
+
+            # For this mock, we'll just return a random question
+            # In a real app, you'd track which questions have been asked in the session
+            return JSONResponse(
+                {
+                    "complete": False,
+                    "question": {"id": seed, "text": prompt},
+                }
+            )
+
+        uvicorn.run(app)
